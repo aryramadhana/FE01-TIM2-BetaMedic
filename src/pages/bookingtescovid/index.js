acd9be75b2ca39bb66797853b9b9e0727ef1d67b
ihsanuddienullah <ihsanuddienullah@gmail.com>
2020-12-16 14:49:20 +0700

    add style to booktescovid

--- src/pages/bookingtescovid/index.js
+++ src/pages/bookingtescovid/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Rsrujukan } from '../../components';
+import './style.css';
 
 const BookingTesCovid = () => {
   return (
-    <div className="container">
+    <div className="page-bookingtescovid">
       <h2>Mencegah Lebih Baik Dari Mengobati</h2>
       <Rsrujukan />
     </div>
