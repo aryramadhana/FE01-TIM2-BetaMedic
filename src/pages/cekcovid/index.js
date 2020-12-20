7cd7c65ca85ea23fef33447a8d3da8cdecc94cb5
sultanrafiraihan <17102165@ittelkom-pwt.ac.id>
2020-12-20 17:39:36 +0700

    fix private page

--- src/pages/cekcovid/index.js
+++ src/pages/cekcovid/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormCovid } from '../../components';
 import './style.css';
 
 const CekCovid = () => {
+  const [isLoginLoading, setLoginLoading] = useState(false);
+
+  useEffect(() => {
+    setLoginLoading(true);
+  }, []);
   return (
     <div className="pages-cekcovid">
-      <h1>Skrinning Mandiri Covid-19</h1>
+      <h1>
+        {isLoginLoading}
+        Skrinning Mandiri Covid-19
+      </h1>
       <FormCovid />
     </div>
   );
