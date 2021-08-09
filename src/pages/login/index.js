f3f359228a6c03c5a0b17c7348bc537e7a9ce710
aryramadhana <ary98ramadhan@gmail.com>
2021-08-09 20:48:19 +0700

    tes Login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -23,7 +23,7 @@ const Login = () => {
       //   .login(email, password)
       .then((res) => {
         // console.log(res.data.token);
-        this.state({
+        this.Setstate({
           datam: res.data,
         });
         console.log(datam);
