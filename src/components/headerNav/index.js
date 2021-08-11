59416e2dcc63ac672bbba6e1af814161e2346313
aryramadhana <ary98ramadhan@gmail.com>
2021-08-11 12:39:33 +0700

    fix login

--- src/components/headerNav/index.js
+++ src/components/headerNav/index.js
@@ -2,6 +2,7 @@
 import { MDBBtn } from 'mdbreact';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.css';
 import { isUserAuthenticated } from '../../utils/cookie';
 
 const Header = () => {
