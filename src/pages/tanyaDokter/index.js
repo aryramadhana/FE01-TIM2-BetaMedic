c59c99058feafc693571af79cd353df6d6d7c64f
aryramadhana <ary98ramadhan@gmail.com>
2021-08-12 17:39:18 +0700

    style tanyaDokter

--- src/pages/tanyaDokter/index.js
+++ src/pages/tanyaDokter/index.js
@@ -40,7 +40,7 @@ const TanyaDokter = () => {
                   <MDBCardImage
                     className="img-thumbnail"
                     src={dokter.foto}
-                    style={{ height: '149px', width: '198px' }}
+                    style={{ height: '149px', width: '246px' }}
                   />
                   <MDBCardTitle className="">
                     <p className="nama">{dokter.nama}</p>
