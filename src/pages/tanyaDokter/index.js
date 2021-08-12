cf7cc9a667157f74b2e90f5fa71f36ca9e4f8ec2
aryramadhana <ary98ramadhan@gmail.com>
2021-08-12 16:27:28 +0700

    style TanyaDokter

--- src/pages/tanyaDokter/index.js
+++ src/pages/tanyaDokter/index.js
@@ -35,9 +35,7 @@ const TanyaDokter = () => {
           const wa = `https://api.whatsapp.com/send?phone=${dokter.no_hp}&text=Betamedic`;
           return (
             <MDBCol style={{ padding: '10px', display: 'flex' }}>
-              <MDBCard
-                style={{ width: '15rem', height: '15rem', margin: '0 auto' }}
-              >
+              <MDBCard style={{ width: '15rem', margin: '0 auto' }}>
                 <MDBCardBody>
                   <MDBCardImage
                     className="img-thumbnail"
