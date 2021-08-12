0c3d555913834f3ccb2d204acab975bdf2d0ec57
aryramadhana <ary98ramadhan@gmail.com>
2021-08-12 16:53:55 +0700

    style TanyaDokter

--- src/pages/tanyaDokter/index.js
+++ src/pages/tanyaDokter/index.js
@@ -39,7 +39,6 @@ const TanyaDokter = () => {
                 <MDBCardBody>
                   <MDBCardImage
                     className="img-thumbnail"
-                    className="mh-100"
                     src={dokter.foto}
                     waves
                   />
