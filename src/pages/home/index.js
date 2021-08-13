86852534d84b29c39e3d0192fc0d007ec864fbef
aryramadhana <ary98ramadhan@gmail.com>
2021-08-13 16:10:15 +0700

    style home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  MDBJumbotron,
-  MDBBtn,
-  MDBCol,
-  MDBCardTitle,
-  MDBIcon,
-  MDBCarousel,
-  MDBCarouselInner,
-  MDBCarouselItem,
-  MDBView,
-  MDBContainer,
-} from 'mdbreact';
+import { MDBJumbotron, MDBBtn, MDBCol, MDBCardTitle, MDBIcon } from 'mdbreact';
 import { CoronaNews } from '../../components';
 import './style.css';
 import { isUserAuthenticated } from '../../utils/cookie';
@@ -21,44 +10,16 @@ const JumbotronPage = () => {
   return (
     <div>
       {isUserAuthenticated() ? (
-        <MDBContainer>
-          <MDBCarousel
-            activeItem={1}
-            length={3}
-            showIndicators={true}
-            className="z-depth-1"
-          >
-            <MDBCarouselInner>
-              <MDBCarouselItem itemId="1">
-                <MDBView>
-                  <img
-                    className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
-                    alt="First slide"
-                  />
-                </MDBView>
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="2">
-                <MDBView>
-                  <img
-                    className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
-                    alt="Second slide"
-                  />
-                </MDBView>
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="3">
-                <MDBView>
-                  <img
-                    className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-                    alt="Third slide"
-                  />
-                </MDBView>
-              </MDBCarouselItem>
-            </MDBCarouselInner>
-          </MDBCarousel>
-        </MDBContainer>
+        <MDBJumbotron style={{ padding: 0 }} className="page-home">
+          <MDBCol className="text-black text-left py-5 px-4">
+            <MDBCol className="py-5">
+              <MDBCardTitle className="h1-responsive pt-3 ml-5 font-bold">
+                Selamat Datang di Beta Medic
+              </MDBCardTitle>
+              <p className="mx-5 mb-5">We Are Here For Your Care</p>
+            </MDBCol>
+          </MDBCol>
+        </MDBJumbotron>
       ) : (
         <MDBJumbotron style={{ padding: 0 }} className="page-home">
           <MDBCol className="text-black text-left py-5 px-4">
